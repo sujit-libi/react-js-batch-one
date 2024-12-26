57f44125217e8851d5984c47f78355de8781abaa
Sujit Kumar Libi <sujit_libi@hotmail.com>
2024-12-26 10:35:27 +0545

    Component Composition

---
+++ Day 22/hamro-netflix/src/Modal.js
@@ -0,0 +1,11 @@
+export default function Modal() {
+  return <div>This is modal</div>;
+}
+
+function SuccessMsg() {
+  return <div>Success Message</div>;
+}
+
+function ErrorMsg() {
+  return <div>Error Message</div>;
+}
---
+++ Day 22/hamro-netflix/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import StarRating from './StarRating';
+// import './index.css';
+// import App from './App';
+import Modal from './Modal';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Modal />
+    {/* <App /> */}
+    {/* <StarRating
+      maxRating={5}
+      message={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating maxRating={5} size={24} /> */}
+  </React.StrictMode>
+);
