064091cf96dfbda9ec4b4cf7562197406df6fee9
Sujit Kumar Libi <sujit_libi@hotmail.com>
2024-12-29 12:15:36 +0545

    built in method of console

---
+++ Day 24/async/async-await.js
@@ -0,0 +1,13 @@
+console.time();
+for (let i = 0; i < 10000; i++) {
+  // console.log(i);
+}
+console.timeLog();
+
+const userObj = {
+  name: 'Sujit',
+  age: 29,
+  email: 'sujitlibi@gmail.com',
+};
+
+console.table(userObj);
