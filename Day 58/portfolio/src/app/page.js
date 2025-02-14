d5e34e75b362a7e76ac90285d391cffbc31a0ec5
Sujit Kumar Libi <sujit_libi@hotmail.com>
2025-02-14 12:23:43 +0545

    Shadcn ui

--- Day 58/portfolio/src/app/page.js
+++ Day 58/portfolio/src/app/page.js
@@ -1,11 +1,13 @@
 import Intro from './components/Intro';
 import TechStack from './components/TechStack';
 import Projects from './components/Projects';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div>
       <Intro />
+      <Button>Click me</Button>
       <TechStack />
       <Projects />
     </div>
